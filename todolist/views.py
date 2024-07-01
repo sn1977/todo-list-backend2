ed4d25ee2b7a5772ff603090a9f640fa28b66e32
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-01 17:06:26 +0200

    chore: Register TodoItem model in admin site and create TodoItemView for retrieving todos

--- todolist/views.py
+++ todolist/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken, APIView, Token, Response
 from rest_framework.response import Response
+from todolist.models import TodoItem
+from todolist.serializers import TodoItemSerializer
 
 
-# Create your views here.
+class TodoItemView(APIView):
+    # authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = []
+
+    def get(self, request, format=None):
+        todos = TodoItem.objects.all()
+        serializer = TodoItemSerializer(todos, many=True)
+        return Response(serializer.data)
+
 
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
+        return Response({"token": token.key, "user_id": user.pk, "email": user.email})
--- todolistbackend2/urls.py
+++ todolistbackend2/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from todolist.views import LoginView
+from todolist.views import LoginView, TodoItemView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view()),
+    path('todos/', TodoItemView.as_view()),
 ]
